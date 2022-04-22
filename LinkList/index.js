58d9965b21f9f143b36f8baf0374a696dafb3cbe
xiaolizi <xiaoliz@qq.com>
2022-04-22 15:59:31 +0800

    feat: linkedlist add indexof

--- LinkList/index.js
+++ LinkList/index.js
@@ -102,7 +102,17 @@ export default class LinkedList {
         return false;
     }
 
-
+    //返回一个元素的位置
+    indexOf(element) {
+        let current = this.head;
+        for ( let i = 0; i < this.count && current != null; i++) {
+            if (this.equalsFn(element, current.element)) {
+                return i;
+            }
+            current = current.next;
+        }
+        return -1;
+    }
 
 
 
