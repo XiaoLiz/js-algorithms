c38c6d4dad3e9e253f540d94e8081a03240ea9ee
xiaolizi <xiaoliz@qq.com>
2022-04-22 16:38:01 +0800

    feat: 双向链表

---
+++ LinkList/DoublyLinkedList.js
@@ -0,0 +1,12 @@
+import { Node }  from './utils/link-list-models.js'
+
+import { LinkedList }  from './LinkedList.js'
+
+
+class DoublyNode extends Node {
+
+}
+
+class DoublyLinkedLit extends LinkedList {
+
+}
